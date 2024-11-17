8ea1e3fef28cc927aee43881cd47149b3cfd9c5d
Secondio <92403924+ViGi9029@users.noreply.github.com>
2024-11-17 23:11:02 +0700

    coba

--- Assets/Script/ButtonInteraction.cs
+++ Assets/Script/ButtonInteraction.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ButtonInteraction : MonoBehaviour
+public class ButtonInteraction : ControlInteractionn
 {
+    [SerializeField] private GameObject uiButton; // The UI Button to show/hide
+    private bool isPlayerNear = false; // Tracks if the player is near the object
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (uiButton != null)
+        {
+            uiButton.SetActive(false); // Hide the button at the start
+        }
+        else
+        {
+            Debug.LogError("UI Button is not assigned in the Inspector!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerNear && Input.GetKeyDown(interactionKey))
+        {
+            ToggleUIButton();
+        }
+    }
+
+    // Toggles the UI button visibility
+    private void ToggleUIButton()
+    {
+        if (uiButton != null)
+        {
+            uiButton.SetActive(!uiButton.activeSelf);
+        }
+    }
+
+    // Detect when the player enters the trigger zone
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    // Detect when the player exits the trigger zone
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+
+            // Ensure the button is hidden when the player leaves
+            if (uiButton != null)
+            {
+                uiButton.SetActive(false);
+            }
+        }
     }
 }
---
+++ Assets/Script/ControlInteractionn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlInteractionn : MonoBehaviour
+{
+    [SerializeField] public KeyCode interactionKey = KeyCode.E; // Key to interact
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
